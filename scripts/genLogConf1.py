50de1bcb92831c71c3296dc4ce5d06ee112043db
Jane Chen <janechen@cs.utexas.edu>
2022-03-29 13:50:10 -0500

    add request rate multiple

--- scripts/genLogConf1.py
+++ scripts/genLogConf1.py
@@ -3,8 +3,7 @@ import json
 import math
 import numpy as np
 
-# multiple is the multiple of request rate
-def genConf(trace1, trace2, rate, multiple):
+def genConf(trace1, trace2, rate):
     with open("FOOTPRINT_DESCRIPTORS/available_fds.txt", "r") as infile:
         for l in infile:
             l = l.strip().split(",")
@@ -16,8 +15,8 @@ def genConf(trace1, trace2, rate, multiple):
     # req_rate_1 = rate
     # req_rate_2 = 100-rate
     
-    req_rate_1 = (1000-rate)/10
-    req_rate_2 = rate/10
+    req_rate_1 = 1
+    req_rate_2 = rate
 
     data = {}
     data["Trace_length"] = "10000000"
@@ -27,17 +26,17 @@ def genConf(trace1, trace2, rate, multiple):
     if req_rate_1 > 0:
         data["Traffic_classes"].append({
             "traffic_class": trace1,
-            "traffic_volume": str(math.floor(t1_orig_req_rate*req_rate_1*multiple))
+            "traffic_volume": str(math.floor(t1_orig_req_rate*req_rate_1))
         })
     if req_rate_2 > 0:
         data["Traffic_classes"].append({
             "traffic_class": trace2,
-            "traffic_volume": str(math.floor(t2_orig_req_rate*req_rate_2*multiple))
+            "traffic_volume": str(math.floor(t2_orig_req_rate*req_rate_2))
         })
 
-    with open('config/tc-0-1-'+str(math.floor(req_rate_1*multiple))+':'+str(math.floor(req_rate_2*multiple))+'.config', 'w') as outfile:
+    with open('config/tc-0-1-'+str(math.floor(req_rate_1))+':'+str(math.floor(req_rate_2))+'.config', 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
-# generate 200 TC-0 & TC-1 traffic mixtures, x is the multiplier ratio of TC-0 and TC-1
-for x in np.logspace(1, 3, 200):
-    genConf("tc-0", "tc-1", x, 5)
+# generate 100 TC-0 & TC-1 traffic mixtures, x is the multiplier ratio of TC-0 and TC-1
+for x in np.logspace(1, 3, 20):
+    genConf("tc-0", "tc-1", x)
