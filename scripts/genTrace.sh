dcee1a7791e1e7929663e48ce7f2ce9c56b1f035
Jane Chen <janechen@cs.wisc.edu>
2022-12-08 05:00:34 -0600

    generate ood traces

--- scripts/genTrace.sh
+++ scripts/genTrace.sh
@@ -3,9 +3,10 @@
 BASEDIR=$(pwd)
 FILES="$BASEDIR/config/*"
 COUNT=0
+mkdir -p /mydata/traces-ood/
 for f in $FILES
     do
-        for ((i=0; i<10; i++))
+        for ((i=0; i<3; i++))
         do
             cp $f ${f::-7}-$i".config"
             echo ${f::-7}-$i".config"
@@ -15,11 +16,11 @@ for f in $FILES
                 then
                     wait
                     COUNT=0
-                    mv OUTPUT/* /mydata/traces/
+                    mv OUTPUT/* /mydata/traces-ood/
             fi
         done
     done
 wait
-mv OUTPUT/* /mydata/traces/
-rm -rf /mydata/traces/debug.txt
-rm -rf /mydata/traces/logfile.txt
+mv OUTPUT/* /mydata/traces-ood/
+rm -rf /mydata/traces-ood/debug.txt
+rm -rf /mydata/traces-ood/logfile.txt
