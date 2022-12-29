77210735e126185fcd4769a1c58d91d84dcbee87
Jane Chen <janechen@cs.wisc.edu>
2022-12-28 23:09:28 -0600

    gen 100M test traces

--- scripts/genTrace.sh
+++ scripts/genTrace.sh
@@ -3,24 +3,24 @@
 BASEDIR=$(pwd)
 FILES="$BASEDIR/config/*"
 COUNT=0
-mkdir -p /mydata/traces-ood/
+mkdir -p /mydata/traces/
 for f in $FILES
     do
-        for ((i=0; i<3; i++))
-        do
-            cp $f ${f::-7}-$i".config"
-            echo ${f::-7}-$i".config"
-            python3 tragen_cli.py -c ${f::-7}-$i".config" &
-            ((COUNT++))
-            if [ $COUNT -eq 4 ]
-                then
-                    wait
-                    COUNT=0
-                    mv OUTPUT/* /mydata/traces-ood/
-            fi
-        done
+        # for ((i=0; i<3; i++))
+        # do
+        #     cp $f ${f::-7}-$i".config"
+        #     echo ${f::-7}-$i".config"
+        python3 tragen_cli.py -c ${f::-7}".config" &
+        ((COUNT++))
+        if [ $COUNT -eq 4 ]
+            then
+                wait
+                COUNT=0
+                mv OUTPUT/* /mydata/traces/
+        fi
+        # done
     done
 wait
-mv OUTPUT/* /mydata/traces-ood/
-rm -rf /mydata/traces-ood/debug.txt
-rm -rf /mydata/traces-ood/logfile.txt
+mv OUTPUT/* /mydata/traces/
+rm -rf /mydata/traces/debug.txt
+rm -rf /mydata/traces/logfile.txt
